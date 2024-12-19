8d9ccd3cb1ec466ef996aeefc1b9ba5fbce0e34f
dosxana@gmail.com <dosxana@gmail.com>
2024-12-19 20:03:13 +0500

    Реализаци сервиса profile-service, реализация файла docker-compose

--- profile_service/docker-entrypoint.sh
+++ profile_service/docker-entrypoint.sh
@@ -6,8 +6,8 @@ set -e
 echo "FASTAPI_ENV: $FASTAPI_ENV"
 if [ "$FASTAPI_ENV" = "production" ]; then
     echo "Starting the production server"
-    exec gunicorn --worker-class uvicorn.workers.UvicornWorker --config app/core/gunicorn_conf.py app.main:app
+    exec gunicorn --worker-class uvicorn.workers.UvicornWorker --config profile_service/core/gunicorn_conf.py profile_service.main:app
 else
     echo "Starting the development server"
-    exec uvicorn --reload --host=${PROFILE_API_UVICORN_HOST:-0.0.0.0} --port=${PROFILE_API_UVICORN_PORT:-8084} app.main:app
+    exec uvicorn --reload --host=${PROFILE_API_UVICORN_HOST:-0.0.0.0} --port=${PROFILE_API_UVICORN_PORT:-8084} profile_service.main:app
 fi
