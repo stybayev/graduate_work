2362f39c429f19992d74ac69a4eafb1020ebb117
dosxana@gmail.com <dosxana@gmail.com>
2024-12-20 19:01:25 +0500

    Добавил файлы alembic для profile service

--- profile_service/alembic/env.py
+++ profile_service/alembic/env.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from core.config import settings
 import models
-from models.users import User
+from models.profiles import UserProfile
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = User.metadata
+target_metadata = UserProfile.metadata
 
 
 # other values from the config, defined by the needs of env.py,
---
+++ profile_service/alembic/versions/3df764df8196_create_profiles_table.py
@@ -0,0 +1,43 @@
+"""New Migration
+
+Revision ID: 70a851e24518
+Revises:
+Create Date: 2024-06-26 07:17:05.290024
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+
+from alembic import op
+import uuid
+
+# revision identifiers, used by Alembic.
+revision: str = "3df764df8196"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    # Create schema
+    op.execute('CREATE SCHEMA IF NOT EXISTS profiles_service')
+
+    # Create table
+    op.create_table(
+        'profiles',
+        sa.Column('id', sa.dialects.postgresql.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True),
+        sa.Column('user_id', sa.dialects.postgresql.UUID(as_uuid=True), nullable=False, unique=True),
+        sa.Column('phone_number', sa.String(length=15), nullable=False, unique=True),
+        sa.Column('full_name', sa.String(length=255), nullable=False),
+        sa.Column('created_at', sa.DateTime, default=sa.func.now()),
+        schema='profiles_service'
+    )
+
+
+def downgrade():
+    # Drop table
+    op.drop_table('profiles', schema='profiles_service')
+
+    # Drop schema
+    op.execute('DROP SCHEMA IF EXISTS profiles_service CASCADE')
