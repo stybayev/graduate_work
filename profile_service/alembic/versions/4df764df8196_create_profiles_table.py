8c4fcfaf38cfd1b8b9ff694cf8d8f124d091a277
dosxana <dosxana@gmail.com>
2024-12-21 20:47:48 +0500

    Добавил контейнер с инструкциями создания таблиц для профиль сервиса

--- profile_service/alembic/versions/4df764df8196_create_profiles_table.py
+++ profile_service/alembic/versions/4df764df8196_create_profiles_table.py
@@ -13,7 +13,7 @@ from alembic import op
 import uuid
 
 # revision identifiers, used by Alembic.
-revision: str = "3df764df8196"
+revision: str = "4df764df8196"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- profile_service/docker-entrypoint.sh
+++ profile_service/docker-entrypoint.sh
@@ -2,12 +2,17 @@
 
 set -e
 
-# Проверяем окружение и запускаем соответствующий сервер
+# Если передана команда мигарции alembic, выполнить её напрямую
+if [ "$1" = "alembic" ]; then
+    exec "$@"
+fi
+
+# Иначе запускаем сервер как обычно
 echo "FASTAPI_ENV: $FASTAPI_ENV"
 if [ "$FASTAPI_ENV" = "production" ]; then
     echo "Starting the production server"
-    exec gunicorn --worker-class uvicorn.workers.UvicornWorker --config profile_service/core/gunicorn_conf.py profile_service.main:app
+    exec gunicorn --worker-class uvicorn.workers.UvicornWorker --config core/gunicorn_conf.py profile_service.main:app
 else
     echo "Starting the development server"
-    exec uvicorn --reload --host=${PROFILE_API_UVICORN_HOST:-0.0.0.0} --port=${PROFILE_API_UVICORN_PORT:-8084} profile_service.main:app
+    exec uvicorn --reload --host=${PROFILE_API_UVICORN_HOST:-0.0.0.0} --port=${PROFILE_API_UVICORN_PORT:-8084} main:app
 fi
