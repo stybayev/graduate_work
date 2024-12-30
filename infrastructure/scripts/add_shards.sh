51254782bfe7999eb4bf99589c36311caf4bb56d
dosxana@gmail.com <dosxana@gmail.com>
2024-12-30 19:31:06 +0500

    Сократил конфиги mongo до одного шарда

--- infrastructure/scripts/add_shards.sh
+++ infrastructure/scripts/add_shards.sh
@@ -2,7 +2,3 @@
 until mongosh --host mongos1 --eval "sh.addShard(\"mongors1/mongors1n1\")"; do
   sleep 5
 done
-
-until mongosh --host mongos1 --eval "sh.addShard(\"mongors2/mongors2n1\")"; do
-  sleep 5
-done
