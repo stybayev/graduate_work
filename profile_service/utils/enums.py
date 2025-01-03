4ef2c2844324361bfb7d7b23cabf02c9567f041a
dosxana <dosxana@gmail.com>
2025-01-03 21:08:52 +0500

    Добавил инициализацию коллекции и составные ключи шардирования

--- profile_service/utils/enums.py
+++ profile_service/utils/enums.py
@@ -1,10 +1,15 @@
 from enum import Enum
 
+
 class ShardedCollections(Enum):
-    LIKES_COLLECTION = ("likesCollection", {"movie_id": "hashed"})
-    REVIEW_COLLECTION = ("reviewCollection", {"movie_id": "hashed"})
-    REVIEW_LIKES_COLLECTION = ("reviewLikesCollection", {"review_id": "hashed"})
+    # Закладки/избранные фильмы пользователей
     BOOKMARKS_COLLECTION = ("bookmarksCollection", {"user_id": "hashed", "movie_id": 1})
+
+    # Рейтинги фильмов от пользователей
+    RATINGS_COLLECTION = ("ratingsCollection", {"movie_id": "hashed", "user_id": 1})
+
+    # Рецензии на фильмы
+    REVIEWS_COLLECTION = ("reviewsCollection", {"movie_id": "hashed", "created_at": -1})
 
     def __init__(self, collection_name, shard_key):
         self.collection_name = collection_name
